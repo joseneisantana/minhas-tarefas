e64bee37ffc1a0148674461542c93b4387da2f53
joseneisantana <joseneisantana@yahoo.com>
2025-01-14 22:29:23 -0300

    instalação das dependencias do projeto

---
+++ src/container/BarraLateral/index.tsx
@@ -0,0 +1,19 @@
+import FiltroCard from '../../components/FiltroCard'
+import * as S from './style'
+const BarraLateral = () => (
+  <S.Aside>
+    <div>
+      <input type="text" placeholder="Buscar" />
+      <S.Filtros>
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard ativo />
+      </S.Filtros>
+    </div>
+  </S.Aside>
+)
+
+export default BarraLateral
